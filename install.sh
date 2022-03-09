c0d4c1072a7a3d2fa0796d40af500b90fd1d8b36
ABC10946 <abctech10946@gmail.com>
2022-03-09 17:56:43 +0900

    initial commit

---
+++ install.sh
@@ -0,0 +1,4 @@
+if [ -d $HOME/.local/bin ]; then
+	cp env_data_getter.py $HOME/.local/bin/env_data_getter.py
+fi
+
---
+++ main.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+from flask import Flask, jsonify
+app = Flask(__name__)
+import subprocess
+import datetime
+
+@app.route('/')
+def env_greet():
+    env_info_raw = subprocess.getoutput('/usr/bin/python3 /home/pi/.local/bin/env_data_getter.py')
+    temp, press, humid = map(float, env_info_raw.split('\n'))
+    dt_now = datetime.datetime.now()
+    timestamp = dt_now.strftime('%Y-%m-%d-%H-%M-%S')
+
+    env_dict = {'timestamp': timestamp, 'temp': temp, 'press': press, 'humid': humid}
+    return jsonify(env_dict)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
